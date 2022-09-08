f8eac82b942586998114ba00e3af02f237dca270
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-09-08 20:42:15 -0300

    alteraçoes e adiçoes basicas componentes criados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from './components/Navbar';
+import Pokedex from './components/Pokedex';
+import Searchbar from './components/Searchbar';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Searchbar />
+      <Pokedex />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav>
+    <nav className='flex items-center justify-evenly flex-row h-24'>
       <div>
-        <img alt="PokéAPI" src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" />
+        <img
+          className='w-40'
+          alt="PokéAPI"
+          src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+        />
       </div>
     </nav>
   )
