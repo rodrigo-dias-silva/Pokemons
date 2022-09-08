c6eef4165667e8f7c049b818ceb68336ead2876c
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-09-08 20:41:03 -0300

    criaçao elemento e configuraçao de inputs e buttons

---
+++ src/components/Searchbar.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import { searchPokemon } from '../api'
+
+function Searchbar() {
+  const [search, setSearch] = useState('')
+  const [pokemon, setPokemon] = useState();
+
+  function onChangeHandler(e) {
+    setSearch(e.target.value)
+  }
+
+  function onButtonClickHandler() {
+    onSearchHandler(search)
+  }
+
+  const onSearchHandler = async (pokemon) => {
+    const result = await searchPokemon(pokemon);
+    setPokemon(result)
+  }
+
+  return (
+    <div className='flex px-5 items-center gap-5'>
+      <div>
+        <input
+          className='p-3 bg-white rounded shadow-md outline-none'
+          placeholder='Buscar pokemon...'
+          onChange={onChangeHandler}
+        />
+      </div>
+      <div>
+        <button
+          className='bg-cyan-600 rounded h-11 text-white px-3 shadow-md '
+          onClick={onButtonClickHandler}
+        >
+          Buscar
+        </button>
+      </div>
+      {pokemon ? (
+        <div>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        </div>
+      ) : null}
+    </div>
+  )
+}
+
+export default Searchbar
