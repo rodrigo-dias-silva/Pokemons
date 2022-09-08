09c8c11a497e22deed9c96387644791a23213130
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-09-08 20:39:22 -0300

    criaçao elemento exibiçao lista pokemons

---
+++ src/components/Pokedex.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Pokedex() {
+  return (
+    <div>
+      <div>
+        <h1>Pokedex</h1>
+      </div>
+      <div>
+        <span>Página: </span>
+      </div>
+    </div>
+  )
+}
+
+export default Pokedex
