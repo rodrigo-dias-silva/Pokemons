ad8a94ad883d9f2708044751dbf49d72046dd736
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-09-10 04:01:46 -0300

    config funcionalidades e design do card + css

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,13 +1,43 @@
+import { Heart } from 'phosphor-react';
 import React from 'react'
 
 function Pokemon(props) {
   const { pokemon } = props
+  const onHeartClick = () => {
+    console.log("pode favoritar");
+  }
+
   return (
-    <div>
+    <div className='flex shadow-md rounded gap-3 p-2'>
       <div>
-        <img alt={pokemon.name} src={pokemon.sprites.front_default} />
+        <img className='w-16 h-16' alt={pokemon.name} src={pokemon.sprites.front_default} />
       </div>
-      <div>{pokemon.name}</div>
+      <div className='flex flex-col justify-between p-2 flex-1'>
+        <div className='flex justify-between items-center'>
+          <span className='capitalize font-bold'>{pokemon.name}</span>
+          <span>#{pokemon.id}</span>
+        </div>
+        <div className='flex flex-row items-center justify-between'>
+          <div className='flex gap-3'>
+            {pokemon.types.map((type, index) => {
+              return (
+                <div
+                  className='capitalize'
+                  key={index}
+                >
+                  {type.type.name}
+                </div>
+              )
+            })}
+          </div>
+          <button
+            onClick={onHeartClick}
+          >
+            <Heart size={16} />
+            <Heart size={16} weight={"fill"} />
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
