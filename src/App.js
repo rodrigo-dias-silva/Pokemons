25843a474dfaa97a05031ba73c82ee48485416e1
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-09-10 02:23:31 -0300

    configuraçao do elemento pokedex.js e promises

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { getPokemonData, getPokemons } from './api';
 import Navbar from './components/Navbar';
 import Pokedex from './components/Pokedex';
 import Searchbar from './components/Searchbar';
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true);
+      const data = await getPokemons();
+      const promises = data.results.map(async (pokemon) => {
+        return await getPokemonData(pokemon.url);
+      });
+
+      const results = await Promise.all(promises);
+
+      setPokemons(results);
+      setLoading(false);
+    } catch (error) {
+      console.log("fetchPokemon error: ", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPokemons();
+  }, [])
+
   return (
     <div>
       <Navbar />
       <Searchbar />
-      <Pokedex />
+      <Pokedex
+        pokemons={pokemons}
+        loading={loading}
+      />
     </div>
   );
 }
